0f4c85ca9ae70b6e9fbd7e75b7cc5e4df4e0973d
Tony Phipps <tony.phipps@gmail.com>
2015-12-12 01:28:25 +0000

    added boilerplate script to scripts.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,7 +4,41 @@
 
   $(function () {
 
-    undefined;
+    /**
+     * Global variables
+     */
+
+    var undefined;
+
+
+
+    /**
+     * Functions
+     */
+
+    var functionName = function () {
+
+    };
+
+
+
+    /**
+     * Initialise function when the document is ready
+     */
+
+    $(document).ready(function (){
+
+    });
+
+
+
+    /**
+     * Initialise function when the window has loaded
+     */
+
+    $(window).load(function () {
+
+    });
 
   });
 
